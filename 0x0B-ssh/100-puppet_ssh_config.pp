db5131236e38c097d81be6b198d695b0371f98a0
GodwinCyber <godwinchuks032@gmail.com>
2024-02-25 12:23:52 -0800

    sudo puppet apply 100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# using Puppet to make changes to our configuration file. 
+
+file_path { 'Authentications':
+             ensure => 'present',
+             path => '/etc/ssh/ssh_config'
+content => "
+             #SSH configuration
+             HOST*
+             PasswordAuthentication no
+             IdentityFile ~/.ssh/school
+            ",
+}
