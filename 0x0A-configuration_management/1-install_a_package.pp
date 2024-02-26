97f44f44a4fa1bd3495650fe1bc3e0b356f42531
GodwinCyber <godwinchuks032@gmail.com>
2024-02-26 13:39:27 -0800

    using puppet to install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Use Puppet to install flash from pip3
-
-package {'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3',
+# Using Puppet, install flask from pip3
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
