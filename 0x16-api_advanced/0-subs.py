93fe785cc895bd9f394a7f6157dfe54cca880d97
GodwinCyber <godwinchuks032@gmail.com>
2024-04-10 11:36:23 +0000

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,18 +4,13 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API for the number of subscribers for a given subreddit.
-    If the subreddit is invalid, returns 0.
-    """
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "python:reddit.subscriber.counter:v1.0 \
-        (by HousingBorn8812)"
-        }
-
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    try:
-        return response.json().get('data').get('subscribers')
-    except Exception:
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
